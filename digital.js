5244b74d7d22c9861552cede6c8d078ce4364729
thanosda <154046417+Thanosda@users.noreply.github.com>
2025-10-28 16:19:17 +0530

    finished

---
+++ digital.js
@@ -0,0 +1,11 @@
+let hrs= document.getElementById("hours");
+let mins= document.getElementById("minutes");
+let secs= document.getElementById("seconds");
+
+
+setInterval(()=>{ let currentTime= new Date();
+
+hrs.innerHTML= (currentTime.getHours())<10 ? "0"+currentTime.getHours() : currentTime.getHours();
+mins.innerHTML= (currentTime.getMinutes())<10 ? "0"+currentTime.getMinutes() : currentTime.getMinutes();
+secs.innerHTML= (currentTime.getSeconds())<10 ? "0"+currentTime.getSeconds() : currentTime.getSeconds();;
+}, 1000);
